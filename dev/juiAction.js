ffe1219cef10d36fdc27760ad4351359694ee25c
Michael Braun <michaelsoftware1997@gmail.com>
2016-10-02 15:26:21 +0200

    Added caller for actions

--- dev/juiAction.js
+++ dev/juiAction.js
@@ -3,28 +3,42 @@
     var actions = [];
 
 	action.call = function(action) {
+        console.log(action);
+
         name = action.replace(/((?:.?)*)\(((?:.?)*)\)/, '$1').toUpperCase(); //(?<!\\)(?:\\{2})*\K"
 
         values = action.replace(/((?:.?)*)\(((?:.?)*)\)/, '$2');
-        values = values.replace(/(?:[[:blank:]]*),/g, ',').replace(/,(?:[[:blank:]]*)/g, ',');
+        //values = values.replace(/(?:[[:blank:]]*),/g, ',').replace(/,(?:[[:blank:]]*)/g, ',');
 
+        values = (' ' + values).replace(/([^(\\)])""/g, '$1');
         values = (' ' + values).replace(/([^(\\)])"/g, '$1');  // RegEx: /(?<!\\)(?:\\{2})*\K"/g
-        values = values.replace(/([^(\\)])'/g, '$1');
-        values = values.replace(/,/g, '$1');
+
+        values = (' ' + values).replace(/([^(\\)])''/g, '$1');
+        values = (' ' + values).replace(/([^(\\)])'/g, '$1');
+        values = values.replace(/(^\s*')|('\s*$)/g, '');
+        //values = values.replace(/,/g, '$1');
         values = values.trim();
         values = values.split(',');
 
         if(!window.jui.tools.empty(actions))
         for(var i = 0, x = actions.length; i < x; i++) {
             if(actions[i].name === name) {
+                console.log(actions[i].name, name, values);
                 actions[i].callback.apply(window, values);
                 break;
             }
         }
 	};
 
-    action.addAction = function(name, callback) {
+    action.caller = function(action) {
+        return function (event) {
+            event.preventDefault();
 
+            window.jui.action.call(action);
+        }
+    };
+
+    action.addAction = function(name, callback) {
         if(window.jui.tools.isFunction(callback)) {
             actions.push({
                 name: name.toUpperCase(),
